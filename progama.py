ce53d487258d9c0325a21fad3cb58cf584026c59
penguindavid <davidmacias@colegioguanenta.edu.co>
2025-02-06 16:25:57 -0500

    se actualizo

--- progama.py
+++ progama.py
@@ -1,4 +1,22 @@
-# input
-r = input("ingrese el tipo de operacion")
-r = input("ingrese el valor de x")
-r = input("ingrese el valor de y")
+input 
+
+import math 
+print("------------------------")
+print("1)suma")
+print("2)resta")
+print("3)multiplicacion")
+print("4)divizion")
+print("5)potencia")
+print("6)logaritmo ")
+print("------------------------")
+
+x = opc =int(input)("digite el primer numero de la operacion ")
+y = opc =int(input)("digite el segundo numero de la operacion")
+
+x = int(input("digite el primer numero de la opercacion"))
+y = int(input("digite el segundo numero de la operacion"))
+
+if opc == 1:
+    print(f"el resultado de la suma es {(x)+(y)}")
+
+
