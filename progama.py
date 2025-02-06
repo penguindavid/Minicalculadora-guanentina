d0428da820e7844a96ca129a70227064b33e9ec5
penguindavid <davidmacias@colegioguanenta.edu.co>
2025-02-06 17:06:08 -0500

    se agrego archivo tipo 2

--- progama.py
+++ progama.py
@@ -10,13 +10,27 @@ print("5)potencia")
 print("6)logaritmo ")
 print("------------------------")
 
-x = opc =int(input)("digite el primer numero de la operacion ")
-y = opc =int(input)("digite el segundo numero de la operacion")
+opc =int(input("digite la operacion a realizar: "))
 
-x = int(input("digite el primer numero de la opercacion"))
-y = int(input("digite el segundo numero de la operacion"))
+x = int(input("digite el primer numero de la operacion: "))
+y = int(input("digite el segundo numero de la operacion: "))
 
 if opc == 1:
     print(f"el resultado de la suma es {(x)+(y)}")
+
+if opc == 2:
+    print(f"el resultado de la resta es {(x)-(y)}")
+
+if opc == 3:
+    print(f"el resultado de la multiplicacion es {(x)*(y)}")
+
+if opc == 4:
+    print(f"el resultado de la division es {(x)/(y)}")
+
+if opc == 5:
+    print(f"el resulado de la potencia es {(x)**(y)}")
+
+if opc == 6:
+    print("El valor es: ", math.log(x, y))
 
 
